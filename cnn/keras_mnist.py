5feb84c32accf1dde0d9a3ab3dfd2d9bc6de6ab4
mhasnat <mhasnat@gmail.com>
2017-11-15 13:03:52 +0100

    modify

--- cnn/keras_mnist.py
+++ cnn/keras_mnist.py
@@ -15,30 +15,26 @@ Gets to 99.25% test accuracy after 12 epochs
 
 import keras
 from keras.datasets import mnist
-from keras.models import Sequential
+from keras.models import Model, Input
 from keras.layers import Dense, Dropout, Flatten
 from keras.layers import Conv2D, MaxPooling2D
 from keras import backend as K
 from keras.layers.normalization import BatchNormalization
 
+import numpy as np
+
 batch_size = 128
 num_classes = 10
-epochs = 12
+epochs = 1
 
 # input image dimensions
 img_rows, img_cols = 28, 28
 
 # the data, shuffled and split between train and test sets
 (x_train, y_train), (x_test, y_test) = mnist.load_data()
-
-if K.image_data_format() == 'channels_first':
-    x_train = x_train.reshape(x_train.shape[0], 1, img_rows, img_cols)
-    x_test = x_test.reshape(x_test.shape[0], 1, img_rows, img_cols)
-    input_shape = (1, img_rows, img_cols)
-else:
-    x_train = x_train.reshape(x_train.shape[0], img_rows, img_cols, 1)
-    x_test = x_test.reshape(x_test.shape[0], img_rows, img_cols, 1)
-    input_shape = (img_rows, img_cols, 1)
+x_train = x_train.reshape(x_train.shape[0], img_rows, img_cols, 1)
+x_test = x_test.reshape(x_test.shape[0], img_rows, img_cols, 1)
+input_shape = (img_rows, img_cols, 1)
 
 x_train = x_train.astype('float32')
 x_test = x_test.astype('float32')
@@ -52,18 +48,20 @@ print(x_test.shape[0], 'test samples')
 y_train = keras.utils.to_categorical(y_train, num_classes)
 y_test = keras.utils.to_categorical(y_test, num_classes)
 
-model = Sequential()
-model.add(Conv2D(32, kernel_size=(3, 3),
-                 activation='relu',
-                 input_shape=input_shape))
-model.add(Conv2D(64, (3, 3), activation='relu'))
-model.add(MaxPooling2D(pool_size=(2, 2)))
-model.add(Dropout(0.25))
-model.add(Flatten())
-model.add(Dense(128, activation='relu'))
-model.add(BatchNormalization())
-model.add(Dropout(0.5))
-model.add(Dense(num_classes, activation='softmax'))
+img_input = Input(shape=input_shape)
+
+c1 = Conv2D(32, kernel_size=(3, 3),activation='relu',input_shape=input_shape)(img_input)
+c2 = Conv2D(64, (3, 3), activation='relu')(c1)
+p1 = MaxPooling2D(pool_size=(2, 2))(c2)
+
+do1 = Dropout(0.25)(p1)
+flat = Flatten()(do1)
+act1 = Dense(128, activation='relu')(flat)
+bn1 = BatchNormalization()(act1)
+do2 = Dropout(0.5)(bn1)
+fc_cl = Dense(num_classes, activation='softmax')(do2)
+
+model = Model(inputs=img_input, outputs = fc_cl)
 
 model.compile(loss=keras.losses.categorical_crossentropy,
               optimizer=keras.optimizers.Adadelta(),
@@ -74,6 +72,7 @@ model.fit(x_train, y_train,
           epochs=epochs,
           verbose=1,
           validation_data=(x_test, y_test))
+
 score = model.evaluate(x_test, y_test, verbose=0)
 print('Test loss:', score[0])
 print('Test accuracy:', score[1])
